6b8461c56b23be326da94d67028e9c0bdec8f32c
himabindu08 <42699277+himabindu08@users.noreply.github.com>
2020-07-22 11:47:37 -0500

    CRUD

---
+++ ppmtool/ppmtool/src/main/java/com/info/repository/ProjectRepository.java
@@ -0,0 +1,15 @@
+package com.info.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.info.domain.Project;
+
+@Repository
+public interface ProjectRepository extends CrudRepository<Project, Long>{
+
+	Project findByProjectIdentifier(String projectId);
+	
+	@Override
+	Iterable<Project> findAll();
+}
